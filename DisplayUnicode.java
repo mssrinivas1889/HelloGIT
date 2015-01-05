282bc21d9a45ab21adc74d831deaaeabbab25280
mssrinivas1889 <mssrinivas1889@gmail.com>
2015-01-05 14:35:47 -0500

    DisplayUnicode.java
    Simple Unicode which displays Telugu Character Numbers

---
+++ DisplayUnicode.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class DisplayUnicode{
+  public static void main(String[] args) {
+    JOptionPane.showMessageDialog(null,
+                                     "\u0C68 \u0C69 \u0C70 \u0C71 \u0C72 ",
+                                     "Numbers",
+                                     JOptionPane.INFORMATION_MESSAGE);
+    }
+  }
+  
